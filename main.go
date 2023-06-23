79f728f9bd43f7f50a757ae7afc1c680bbb0fd31
Julian Alvarez <juliancamiloalvarez77@gmail.com>
2023-06-23 14:57:17 -0500

    Get cli args not with scanner

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"os"
 	"toDoList/helpers"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
 	router := helpers.Setup()
-	for true {
-		command := helpers.ReadCommand(scanner)
-		router.Router(command)
-	}
+	router.Router(os.Args[1:])
 }
